3f2f8f3ea5cd785ca04d1a4c69f57d90ccfc4bab
neil <mcgrogan.neil@gmail.com>
2021-11-04 00:35:45 -0500

    add build files

---
+++ GPA Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GPA Calculator
+//
+//  Created by Neil McGrogan on 11/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GPA Calculator/GPA_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPA_CalculatorApp.swift
+//  GPA Calculator
+//
+//  Created by Neil McGrogan on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct GPA_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
